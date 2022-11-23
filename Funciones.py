31bcddd745a262d0668d3f96a665786fca605af7
Rotciv1092 <117726416+Rotciv1092@users.noreply.github.com>
2022-11-23 17:28:12 +0100

    Update Funciones.py

--- Funciones.py
+++ Funciones.py
@@ -1,3 +1,17 @@
 def dni(numero):
   letras="TRWAGMYFPDXBNJZSQVHLCKE"
   return "{}".format(letras[numero%23])
+
+def comprobarLuhn(numero):
+  comprobante=0
+  for i in range (16):
+    valor=int(numero[i])
+    if i % 2 == 0:
+      valor=int(numero[i])*2
+      if valor >= 10:
+        valor=valor-9
+    comprobante = comprobante+valor
+  if comprobante %10 == 0:
+    return True
+  else:
+    return False
